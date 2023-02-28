a96c123acf5afde1f9ccda49c05d0e9022fdece6
yashmantoo <109843089+yashmantoo@users.noreply.github.com>
2023-02-28 10:25:30 +0000

    added user model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
-import mongoose from "mongoose";
+import mongoose, { model } from "mongoose";
+import validatorPackage from "validator"
+import AuthRoles from "../utils/authRoles"
 
 const userSchema = mongoose.Schema(
     {
@@ -6,6 +8,35 @@ const userSchema = mongoose.Schema(
             type: String,
             required: [true, "Nmae is required."],
             maxLength: [25, "Name should not occupy more than 25 characters."]
-        }
-    }
+        },
+        email: {
+            type: String,
+            required: [true, "Email is required."],
+            unique: true,
+            validate: {
+                validator: validatorPackage.isEmail,
+                message: "Provide a valid Email."
+            }
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required."],
+            minLength: [8, "Password must be atleast 8 characters."],
+            select: false
+        },
+        role:{
+            type: String,
+            enum: Object.values(AuthRoles),
+            default: AuthRoles.USER
+        },
+        forgotPasswordToken: String,
+        ForgotPasswordExpiry: Date,
+
+    },
+
+    {
+        timestamps: true
+    },
 )
+
+export default mongoose.model("User", userSchema)
