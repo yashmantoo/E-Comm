7f8d4b34c66b7fea92a2254c067993cf80375395
yashmantoo <109843089+yashmantoo@users.noreply.github.com>
2023-02-28 10:59:08 +0000

    password encryption added

--- models/user.js
+++ models/user.js
@@ -1,6 +1,9 @@
 import mongoose, { model } from "mongoose";
 import validatorPackage from "validator"
 import AuthRoles from "../utils/authRoles"
+import bcrypt from "bcryptjs"
+import JWT from "jsonwebtoken"
+import crypto from "crypto"
 
 const userSchema = mongoose.Schema(
     {
@@ -38,5 +41,18 @@ const userSchema = mongoose.Schema(
         timestamps: true
     },
 )
+
+// encrypt password
+
+userSchema.pre("save", async function(next){
+    if(!this.modified("password")){return next()}
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+
+
+
+
 
 export default mongoose.model("User", userSchema)
