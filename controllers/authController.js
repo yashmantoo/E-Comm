959d48b0b03f02d3fa23f2b7b54e31f7b3a162c8
yashmantoo <109843089+yashmantoo@users.noreply.github.com>
2023-07-06 08:18:16 +0000

    getProfile controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -209,3 +209,23 @@ export const resetPassword = asyncHandler(async(req, res) => {
 
 })
 
+/******************************************************
+ * @GET_PROFILE
+ * @REQUEST_TYPE GET
+ * @route http://localhost:4000/api/auth/profile
+ * @description check for token and populate req.user
+ * @parameters 
+ * @returns User Object
+ ******************************************************/
+
+export const getProfile = asyncHandler(async(req, res) => {
+    const {user} = req
+    if (!user) {
+        throw new customError('User not found', 404)
+    }
+    res.status(200).json({
+        success: true,
+        user
+    })
+
+})
